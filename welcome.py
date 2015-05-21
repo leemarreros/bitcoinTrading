b20db83aab2cf042b3d103714f552517c9861592
steveleec <lee.marreros@pucp.pe>
2015-05-21 08:00:43 -0700

    Create welcome.py

---
+++ welcome.py
@@ -0,0 +1,8 @@
+__author__ = 'steveleec'
+#welcome.py
+
+def welcome():
+    print "Welcome!"
+    print "You can use this app to find the right moment to invest on Bitcoins."
+    print "We are going to use a Trading Strategy: Move Average Price."
+    print "The algorithm is updated every day and it points to buy or sell Bitcoins."
